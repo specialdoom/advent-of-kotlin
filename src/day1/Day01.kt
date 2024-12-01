cd951612899b716c71bcc899eb216d9bdda7a458
specialdoom <bledea.bogdan97@gmail.com>
2024-12-01 19:53:18 +0200

    chore: day1

---
+++ src/day1/Day01.kt
@@ -0,0 +1,42 @@
+package day1
+
+import println
+import readInput
+import kotlin.math.abs
+
+fun main() {
+    fun splitColumns(input: List<String>): Pair<List<Long>, List<Long>> {
+        return input.map { line ->
+            val first = line.substringBefore(" ").toLong()
+            val second = line.substringAfterLast(" ").toLong()
+            first to second
+        }.unzip()
+    }
+    fun part1(input: List<String>) {
+        val (left, right) = splitColumns(input)
+
+        val result = left.sorted().zip(right.sorted()).sumOf { (first, second) ->
+            abs(first - second)
+        }
+
+        println("Part 1: $result")
+    }
+
+    fun part2(input: List<String>) {
+        val (left, right) = splitColumns(input)
+
+        val frequencies = right.groupingBy{it}.eachCount()
+
+        val result = left.fold(0L){ acc, num ->
+            acc + num * frequencies.getOrDefault(num, 0)
+        }
+
+        println("Part 2: $result")
+    }
+
+
+    val input = readInput("day1/Day01")
+
+    part1(input)
+    part2(input)
+}
