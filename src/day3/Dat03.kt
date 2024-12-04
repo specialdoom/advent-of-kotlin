d81d66bf64a89a7e83947bce5752b6266a160bdf
specialdoom <41910815+specialdoom@users.noreply.github.com>
2024-12-04 11:58:33 +0200

    Update Dat03.kt

--- src/day3/Dat03.kt
+++ src/day3/Dat03.kt
@@ -93,24 +93,47 @@ fun main() {
     }
 
     // PART 1 with regex (something is odd)
-    fun withRegex(input: List<String>): Long {
-        val regex = "mul+\\([\\d{1,3}]+,[\\d{1,3}]+\\)".toRegex()
-        val matchResult = regex.findAll(input.joinToString())
-        val result = matchResult.map{it.value}
-            .toList()
-            .map{it.split("mul(", ",", ")").filter{splitIt -> splitIt.toLongOrNull() != null}.map{filteredIt -> filteredIt.toLong()}}
-            .sumOf { it.reduce{acc, i -> acc * i} }
+    fun withRegex(input: String): Long {
+        val regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)".toRegex()
+        return regex.findAll(input).sumOf{
+            val(first, second) = it.destructured
+            first.toLong() * second.toLong()
+        }
+    }
 
-        return result
+    fun MatchResult.multiplyNumbers(): Long {
+        val (first, second) = destructured
+        return first.toLong() * second.toLong()
+    }
+
+    fun withRegexPart2(input: String): Long {
+        val regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)".toRegex()
+        val doRegex = "do\\(\\)".toRegex()
+        val dontRegex = "don't\\(\\)".toRegex()
+        var sum = 0L
+        var enabled = true
+
+        """$regex|$doRegex|$dontRegex""".toRegex().findAll(input).forEach { match ->
+            when (match.value) {
+                "don't()" -> enabled = false
+                "do()" -> enabled = true
+                else -> if(enabled) sum += match.multiplyNumbers()
+            }
+
+        }
+
+        return sum
     }
 
     fun part1(input: List<String>) {
         val result = parseSection(input.joinToString()).sumOf{it.reduce{acc, i -> i * acc}}
         val result2 = input.sumOf{parseSection(it).sumOf{pair -> pair.reduce{acc, i -> i * acc}}}
-        val withRegexResult = withRegex(input)
+        val withRegexResult = withRegex(input.joinToString())
+        val part2WithRegex = withRegexPart2(input.joinToString())
         println("Part 1 (without joining the string): $result2")
         println("Part 1: $result")
         println("Part 1 (with regex): $withRegexResult")
+        println("Part 2 (with regex): $part2WithRegex")
     }
 
     example()
